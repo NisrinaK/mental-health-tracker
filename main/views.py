b8fb274980681ae62d34e247f64682825e68c475
NisrinaK <nisrina1272@gmail.com>
2024-09-04 13:47:29 +0700

    Berhasil menjalankan Django Test

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def show_main(request):
+    context = {
+        'npm' : '2306275456',
+        'name': 'Nisrina Kamilya Nisyya',
+        'class': 'PBP A'
+    }
+
+    return render(request, "main.html", context)
